edc3e015808292ec8eb19ca99359418baef7f6f1
7anF <7anf2005hanafiah@gmail.com>
2024-08-13 06:01:03 -0700

    Displaying the movies

--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -6,12 +6,38 @@ import {
 } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useGetMoviesQuery } from "../../services/TMDB";
+import { MovieList } from "..";
 
 const MoviesPage = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  return <div>Ahmad</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that className
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "An error has accured";
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default MoviesPage;
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,16 @@ import ActorsInfo from "./ActorsInfo/ActorsInfo";
 import MoviesInfo from "./MoviesInfo/MoviesInfo";
 import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
+import MovieList from "./MovieList/MovieList";
+import Movie from "./Movie/Movie";
 
-export { MoviesPage, ProfilePage, ActorsInfo, MoviesInfo, Navbar, Sidebar };
+export {
+  MoviesPage,
+  ProfilePage,
+  ActorsInfo,
+  MoviesInfo,
+  Navbar,
+  Sidebar,
+  MovieList,
+  Movie,
+};
